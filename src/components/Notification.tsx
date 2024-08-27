054c33b3d5e7b86f9d113244531565b52372f251
Harnoor Singh Aulakh <harnooraulakh16@gmail.com>
2024-08-27 22:22:49 +0530

    multiple notifications showing.types of notifications also added

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -3,58 +3,93 @@ import React from "react";
 import { AnimatePresence, easeIn, motion } from "framer-motion";
 import { useState, useEffect, createContext } from "react";
 import { RxCross2 } from "react-icons/rx";
+import { useContext } from "react";
+
+type Notification = {
+  show: boolean;
+  notification: string;
+  id: number;
+  type: "success" | "error" | "pending";
+};
 
 export const notificationContext = createContext<{
-  data: { show: boolean; notification: string ,id:number};
-  setter: React.Dispatch<
-    React.SetStateAction<{ show: boolean; notification: string;id:number }>
-  >;
+  data: Notification[];
+  setter: React.Dispatch<React.SetStateAction<Notification[]>>;
 }>({
-
-  data: { show: false, notification: "" ,id: Date.now()},
+  data: [],
   setter: () => {},
 });
 
 export function Notifications({ children }: { children: React.ReactNode }) {
-  const [data, setter] = useState({ show: false, notification: "" ,id: Date.now()});
-  useEffect(() => {
-    console.log(data.id);
-    if (!data.show) return;
-    const id1 = setTimeout(() => setter((x) => ({ ...x, show: false })), 5100);
-    return () => clearTimeout(id1);
-    
-  }, [data.show]);
+  const [data, setter] = useState<Notification[]>([]);
   return (
     <notificationContext.Provider value={{ setter, data }}>
       {children}
-      <AnimatePresence key={`${data.notification}-${Date.now()}`}>
+
+      <div className="z-[99999] flex flex-col gap-[2rem] w-max fixed top-[5%] text-black rounded-md right-0">
+        {data.map((x, i) => (
+          <Card
+            key={x.id}
+            show={x.show}
+            notification={x.notification}
+            id={x.id}
+            type={x.type}
+          />
+        ))}
+      </div>
+    </notificationContext.Provider>
+  );
+}
+
+function Card({ show, notification, id, type }: Notification) {
+  const [data, setter] = useState({ show, notification });
+  const [loaderColor, setLoaderColor] = useState("blue");
+
+  
+  useEffect(() => {
+    if(type === "success") {
+      setLoaderColor("bg-[#4cb543]");
+    }
+    if(type === "pending") {
+      setLoaderColor("bg-[#FFBE12]");
+    }
+  
+    if(type === "error") {
+      setLoaderColor("bg-red-700");
+    }
+    const id1 = setTimeout(() => setter((x) => ({ ...x, show: false })), 5100);
+    return () => {
+      clearTimeout(id1);
+    };
+  }, [data.show]);
+  return (
+    <>
+      <AnimatePresence>
         {data.show && (
           <motion.div
-            key={data.id}
             initial={{ x: "100%" }}
             animate={{ x: "-5%" }}
             exit={{ x: "100%" }}
             transition={{ type: easeIn, duration: 0.3 }}
-            className="z-[99999] bg-white w-[20rem] fixed top-[5%] text-black rounded-md right-0"
+            className="z-[99999] bg-white w-[20rem]  text-black rounded-md"
           >
             <RxCross2
               onClick={() => setter((x) => ({ ...x, show: false }))}
               className="absolute top-2 right-2"
             />
             <div className="px-2 pt-5 pb-2">{data.notification}</div>
-
             <div className="w-full overflow-hidden h-[5px] rounded-lg">
               <motion.div
+                key={id}
                 initial={{ x: 0 }}
                 animate={{ x: "-100%" }}
                 transition={{ type: easeIn, duration: 5 }}
-                className="bg-red-700 w-full h-[5px]"
+                className={`${loaderColor} w-full h-[5px]`}
               ></motion.div>
             </div>
           </motion.div>
         )}
       </AnimatePresence>
-    </notificationContext.Provider>
+    </>
   );
-  
 }
--- src/hooks/useNotify.tsx
+++ src/hooks/useNotify.tsx
@@ -1,10 +1,10 @@
 import { useContext } from "react";
 import { notificationContext } from "../components/Notification";
 
-export function useNotify(){
-    const {data,setter} = useContext(notificationContext);
-    function show(notification: string){
-        setter({show: true, notification,id: Date.now()});
-    }
-    return {show,setter, data};
+export function useNotify() {
+  const { data, setter } = useContext(notificationContext);
+  function show(notification: string,type: "success" | "error" | "pending") {
+    setter((x) => [{ show: true, notification, id: Date.now(),type:type },...x]);
+  }
+  return { show, setter, data };
 }
